cf68b0783436d1b4e78f8bbedad1cbd4cbe0ffee
dhakal79 <n.dhakal@un-ihe.org>
2021-08-23 23:14:16 +0000

    add aaray questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,173 @@ nextApp.addEventListener("click", function () {
     instructionBox.setAttribute("class", "quit-class");
 })
 
+// Array of Questions and Answers.
+
+let questions = [{
+    number: 1,
+    question: "Which of the following is a greenhouse gas?",
+    answers: ["CO2", "Methane", "Vapor Water", "Above All"],
+    correctAnswer: "Above All",
+},
+{
+    number: 2,
+    question: "Which of these countries emits the most carbon dioxide?",
+    answers: ["China",
+        "USA",
+        "UK",
+        "Russia"
+    ],
+    correctAnswer: "China",
+},
+{
+    number: 3,
+    question: "Which has been the hottest year on record ?",
+    answers: ["2016",
+        "2020",
+        "All of the above",
+        "None of the above"
+    ],
+    correctAnswer: "All of the above",
+},
+{
+    number: 4,
+    question: "which of the following sectors emits largest % of greenhouse gas?",
+    answers: ["Transportation",
+        "Buildings",
+        "Industry",
+        "Electricity and heat production"
+    ],
+    correctAnswer: "Electricity and heat production",
+},
+{
+    number: 5,
+    question: "How much have sea levels risen in the past 100 years?",
+    answers: ["7 inches", "2 inches", "5 inches", "16 inches"],
+    correctAnswer: "7 inches",
+},
+{
+    number: 6,
+    question: "How could global warming affect polar regions?",
+    answers: ["It will make them warmer",
+        "It will make them colder",
+        "It won't affect them",
+        "None"
+    ],
+    correctAnswer: "It will make them warmer",
+},
+{
+    number: 7,
+    question: "What is solar radiation?",
+    answers: ["Energy from the sun",
+        "Energy from within the Earth",
+        "Energy from the wind",
+        "All of the above"
+    ],
+    correctAnswer: "Energy from within the Earth",
+},
+{
+    number: 8,
+    question: "How long does CO2 remain in the atmosphere?",
+    answers: ["up to 200 years",
+        "up to 5 years",
+        "up to 100 years",
+        "All of the above"
+    ],
+    correctAnswer: "up to 200 years",
+},
+{
+    number: 9,
+    question: "What do scientists agree is causing climate change?",
+    answers: ["Humans",
+        "Animals",
+        "Sun",
+        "All of the above"
+    ],
+    correctAnswer: "Humans",
+},
+{
+    number: 10,
+    question: "What's the easiest ways you can help prevent global warming?",
+    answers: ["Stop Eating Meat",
+        "Use Less Fossil Fuels",
+        "Plant trees",
+        "All of the above"
+    ],
+    correctAnswer: "All of the above",
+},
+{
+    number: 11,
+    question: "How much of the world's food produced in a year is thrown away uneaten?",
+    answers: ["One quarter",
+        "One third",
+        "One half",
+        "All of the above"
+    ],
+    correctAnswer: "One third",
+},
+{
+    number: 12,
+    question: "What is the  biggest threat towards development is?",
+    answers: ["Climate change",
+        "Pollution",
+        "Population growth",
+        "All of the above"
+    ],
+    correctAnswer: "Climate change",
+},
+{
+    number: 13,
+    question: "What percentage of the global greenhouse gas emissions does the transportation sector emit?",
+    answers: ["20%",
+        "14%",
+        "50%",
+        "70%"
+    ],
+    correctAnswer: "14%",
+},
+{
+    number: 14,
+    question: "When you hear it’s unusually hot today, is that about the climate or weather?",
+    answers: ["Climate",
+        "weather",
+        "None",
+        "All of the above"
+    ],
+    correctAnswer: "weather",
+},
+{
+    number: 15,
+    question: " When was the last time in Earth's history that CO2 was as high as it is now?",
+    answers: ["This is the highest it's ever been",
+        "The last time CO2 was this high was 3 million years ago.",
+        "None",
+        "All of the above"
+    ],
+    correctAnswer: "The last time CO2 was this high was 3 million years ago.",
+},
+
+]
+
+// function to getting all the questions and answer options from array .
+
+const questionContent = document.getElementById("question");
+var answerContent = document.getElementById("answer");
+const selectedAnswer = document.getElementsByClassName("answer-option");
+
+function allQuestion(event) {
+    let createTag = document.createElement("p").innerHTML = questions[event].number + ")" + " " + questions[event].question;
+    questionContent.innerHTML = createTag;
+    let answerOption = `<div class="answer-option">` + `<p>` + questions[event].answers[0] + `</P></div>` +
+        `<div class="answer-option">` + `<p>` + questions[event].answers[1] + `</P></div>` +
+        `<div class="answer-option"> ` + `<p>` + questions[event].answers[2] + `</P></div>` +
+        `<div class="answer-option" >` + `<p>` + questions[event].answers[3] + `</P></div>`;
+    answerContent.innerHTML = answerOption;
+
+    for (let x = 0; x < selectedAnswer.length; x++) {
+        selectedAnswer[x].setAttribute("onclick", "selectedAnswers(this)");
+    }
+}
+
+allQuestion(0);
+let currentQuestion = 0;
+let totalScoreAchieved = 0;
