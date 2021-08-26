8ddada397f29a33e83fd1fc1ecdb7ec0284fa02e
dhakal79 <n.dhakal@un-ihe.org>
2021-08-26 17:48:01 +0000

    add animation to headline letters in the index.html page using Js code

--- assets/js/home.js
+++ assets/js/home.js
@@ -6,3 +6,31 @@ const tl = new TimelineMax();
 tl. fromTo(hero, 1, {height: "0%"}, {height: "80%", ease: Power2.easeInOut})
 . fromTo(hero, 1, {width: "100%"}, {width: "80%", ease: Power2.easeInOut})
 . fromTo(headline, 0.5, {opacity: 0, x: 30 }, {opacity: 1, x: 0}, "-=0.5");
+
+// animate the text 
+const text = document.querySelector('.headline')
+const strText =text.textContent;
+const splitText = strText.split("");
+text.textContent ="";
+
+for(let i= 0; i < splitText.length; i++){
+ text.innerHTML += "<span>" + splitText[i] +"</span>";
+
+}
+
+let char = 0;
+let timer = setInterval(onTick, 50);
+function onTick (){
+    const span = text.querySelectorAll('span')[char];
+    span.classList.add('fade');
+    char++
+    if(char ===splitText.length){
+       complete();
+       return;
+    }
+}
+
+function complete(){
+    clearInterval(timer);
+    timer = null;
+}
