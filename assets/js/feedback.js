1d84ca5e63081ee88fc6bd645951b967ccec90c1
dhakal79 <n.dhakal@un-ihe.org>
2021-09-02 22:40:27 +0000

    add javascript code to alret for feedback

---
+++ assets/js/feedback.js
@@ -0,0 +1,14 @@
+
+// Checking form if has value and return alert in case of missing
+
+function checkForm() {
+    let x = document.forms["form"]["comment"].value;
+    if (x == "") {
+      alert("Please leave feedback");
+      return false;
+    }
+}
+
+function clearForm() {
+    document.getElementById("Form").reset();
+}
