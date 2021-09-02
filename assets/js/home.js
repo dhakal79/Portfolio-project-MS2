c2323057526d8dae68f9b23977938de9d943b792
dhakal79 <n.dhakal@un-ihe.org>
2021-09-02 15:57:10 +0000

    debugging headline title in the index.html page +media query

--- assets/js/home.js
+++ assets/js/home.js
@@ -5,7 +5,7 @@ const headline = document.querySelector('.headline')
 const tl = new TimelineMax();
 tl.fromTo(hero, 1, {height: "0%"}, {height: "80%", ease: Power2.easeInOut})
 .fromTo(hero, 1, {width: "100%"}, {width: "80%", ease: Power2.easeInOut})
-.fromTo(headline, 0.5, {opacity: 0, x: 30 }, {opacity: 1, x: 0}, "-=0.5");
+.fromTo(headline, 0.1, {opacity: 0, x: 30 }, {opacity: 1, x: 0}, "-=0.5");
 
 // animate the text 
 const text = document.querySelector('.headline')
@@ -19,7 +19,7 @@ for(let i= 0; i < splitText.length; i++){
 }
 
 let char = 0;
-let timer = setInterval(onTick, 2000);
+let timer = setInterval(onTick, 200);
 function onTick (){
     const span = text.querySelectorAll('span')[char];
     span.classList.add('fade');
